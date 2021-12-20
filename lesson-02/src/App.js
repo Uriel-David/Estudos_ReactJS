a965ea88d149faaf71f7c32301392255a0201bc9
Uriel-David <uriel.david.qaa@gmail.com>
2021-12-20 17:10:10 +0000

    Estudos sobre uso e configuração do Webpack e Eslint com Reactjs.

---
+++ lesson-02/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './Button';
+
+const App = function () {
+  return (
+    <div>
+      <p>Teste de aplicação</p>
+      <p>Bem vindo a página ;D</p>
+      <div>Webpack 5</div>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
---
+++ lesson-02/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = function () {
+  return <button>Olá ;D</button>;
+};
+
+export default Button;
